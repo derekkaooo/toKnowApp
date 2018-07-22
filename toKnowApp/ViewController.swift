187273f373779d563724058630c753fb5eff49e1
Derek <stiau334@gmail.com>
2018-07-22 16:25:43 +0800

    Initial Commit

---
+++ toKnowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toKnowApp
+//
+//  Created by Derek on 2018/7/22.
+//  Copyright © 2018年 Derek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
